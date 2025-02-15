fadab3e85cb0b2eb320059855a25483e91c2199e
ahsam <ah.samba12@gmail.com>
2025-02-15 14:10:26 +0200

    Revert "update 15/4"
    This reverts commit cb0dc1ff7451a78ebecaa5bfd0ee3fc51643031f.

--- lib/ui/screens/splashScreen.dart
+++ lib/ui/screens/splashScreen.dart
@@ -219,7 +219,7 @@ class _SplashScreenState extends State<SplashScreen> {
                       const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                   child: const Center(
                     child: CustomSvgPicture(
-                        svgImage: AppAssets.splashLogonew,
+                        svgImage: AppAssets.splashLogo,
                         height: 240,
                         width: 220),
                   ),
@@ -230,7 +230,7 @@ class _SplashScreenState extends State<SplashScreen> {
                     child: Align(
                       alignment: Alignment.bottomCenter,
                       child: CustomSvgPicture(
-                          svgImage: AppAssets.splashLogonew,
+                          svgImage: AppAssets.splashScreenBottomLogo,
                           width: 135,
                           height: 25),
                     ),
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -26,8 +26,8 @@ extension AppColors on ColorScheme {
   Color get lightGreyColor => const Color(0xff8B8B8B);
 
   //splashScreen GradientColor
-  static Color splashScreenGradientTopColor = const Color(0xffffffff);
-  static Color splashScreenGradientBottomColor = const Color(0xffffffff);
+  static Color splashScreenGradientTopColor = const Color(0xff221400);
+  static Color splashScreenGradientBottomColor = const Color(0xff221400);
 
   Color get primaryColor =>
       brightness == Brightness.light ? lightPrimaryColor : darkPrimaryColor;
